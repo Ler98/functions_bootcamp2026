f7863be74420c5849def1e49ea2687648abfabc1
Ler98 <lisa.eriksson98@hotmail.com>
2026-01-07 23:15:02 +0100

    2a function bootcamp

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// 1.
+// function hellofunction() {
+//     console.log('Hello function');
+// }
+
+// hellofunction ();
+
+2.
+
+let count = 0;
+
+function increase(c) {
+    c = c + 1;
+    return c
+}
+
+count = increase(count);
+
+console.log( count);
